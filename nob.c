d0309f96880ec0db9285394f3066f6b244edca3c
girvel <widauka@ya.ru>
2025-09-14 20:43:28 +0500

    nob: Finish redesign, `./nob test`

--- nob.c
+++ nob.c
@@ -7,9 +7,16 @@
 
 #define BUILD_FOLDER ".build/"
 #define SRC_FOLDER "src/"
+
 #define OUTFILE BUILD_FOLDER"stdlib.so"
 #define SOURCES_N 3
-const char *sources[SOURCES_N] = {SRC_FOLDER"test.c", SRC_FOLDER"string.c", SRC_FOLDER"list.c"};
+#define SOURCES SRC_FOLDER"test.c", SRC_FOLDER"string.c", SRC_FOLDER"list.c"
+const char *sources[SOURCES_N] = {SOURCES};
+
+#define TEST_OUTFILE BUILD_FOLDER"test.out"
+#define TEST_SOURCES_N SOURCES_N + 1
+#define TEST_ENTRYPOINT "test.c"
+const char *test_sources[TEST_SOURCES_N] = {SOURCES, TEST_ENTRYPOINT};
 
 // NEXT CLI:
 // NEXT /include for headers
@@ -24,7 +31,7 @@ bool help(int argc, char **argv) {
 
 bool prep();
 bool build(size_t, const char **, bool);
-bool link_(size_t, const char **, const char *);
+bool link_(size_t, const char **, const char *, bool);
 bool run_tests();
 
 bool run(int argc, char **argv) {
@@ -38,23 +45,23 @@ bool run(int argc, char **argv) {
     if (argc > 3 && strcmp(argv[1], "build") == 0) {
         MUST(prep());
         MUST(build(argc - 3, (const char **)argv + 2, safe));
-        MUST(link_(argc - 3, (const char **)argv + 2, argv[argc - 1]));
+        MUST(link_(argc - 3, (const char **)argv + 2, argv[argc - 1], false));
         return true;
     }
 
     if (argc == 2 && strcmp(argv[1], "build") == 0) {
         return prep()
             && build(SOURCES_N, sources, safe)
-            && link_(SOURCES_N, sources, OUTFILE);
+            && link_(SOURCES_N, sources, OUTFILE, false);
     }
 
-    // if (argc == 2 && strcmp(argv[1], "test") == 0) {
-    //     return prep()
-    //         && build_sources(safe)
-    //         && build_tests()
-    //         && link_tests()
-    //         && run_tests();
-    // }
+    if (argc == 2 && strcmp(argv[1], "test") == 0) {
+        MUST(prep());
+        MUST(build(TEST_SOURCES_N, test_sources, safe));
+        MUST(link_(TEST_SOURCES_N, test_sources, TEST_OUTFILE, true));
+        MUST(run_tests());
+        return true;
+    }
 
     return help(argc, argv);
 }
@@ -95,7 +102,7 @@ bool build(size_t sources_n, const char **source_paths, bool safe) {
     return true;
 }
 
-bool link_(size_t sources_n, const char **sources, const char *outfile) {
+bool link_(size_t sources_n, const char **sources, const char *outfile, bool is_exe) {
     Nob_Cmd cmd = {0};
     nob_cc(&cmd);
     Nob_String_Builder *object_files = calloc(sources_n, sizeof(Nob_String_Builder));
@@ -103,7 +110,7 @@ bool link_(size_t sources_n, const char **sources, const char *outfile) {
         object_files[i] = src_to_obj(sources[i]);
         nob_cmd_append(&cmd, object_files[i].items);
     }
-    nob_cmd_append(&cmd, "-shared");
+    if (!is_exe) nob_cmd_append(&cmd, "-shared");
     nob_cc_output(&cmd, outfile);
 
     bool result = nob_cmd_run(&cmd);
@@ -117,43 +124,10 @@ bool link_(size_t sources_n, const char **sources, const char *outfile) {
 }
 
 bool run_tests() {
-    
+    Nob_Cmd cmd = {0};
+    nob_cmd_append(&cmd, "./"TEST_OUTFILE);
+    return nob_cmd_run(&cmd);
 }
-
-// bool step_build(bool safe) {
-//     nob_log(NOB_INFO, "  BUILD STAGE");
-// 
-//     MUST(nob_mkdir_if_not_exists(BUILD_FOLDER));
-//     MUST(nob_write_entire_file(BUILD_FOLDER".gitignore", "*", 1));
-// 
-//     Nob_Cmd cmd = {0};
-//     nob_cc(&cmd);
-//     nob_cc_flags(&cmd);
-//     nob_cc_inputs(&cmd,
-//         "test.c",
-//         SRC_FOLDER"list.c",
-//         SRC_FOLDER"test.c",
-//         SRC_FOLDER"string.c"
-//     );
-//     nob_cc_output(&cmd, BUILD_FOLDER"test.o");
-//     if (safe) {
-//         nob_cmd_append(&cmd, "-DSTD_SAFETY=1");
-//     }
-// 
-//     MUST(nob_cmd_run(&cmd));
-// 
-//     return true;
-// }
-// 
-// bool step_run() {
-//     nob_log(NOB_INFO, "  RUN STAGE");
-// 
-//     Nob_Cmd cmd = {0};
-//     nob_cmd_append(&cmd, "./"BUILD_FOLDER"test.o");
-//     MUST(nob_cmd_run(&cmd));
-// 
-//     return true;
-// }
 
 int main(int argc, char **argv) {
     return !run(argc, argv);
